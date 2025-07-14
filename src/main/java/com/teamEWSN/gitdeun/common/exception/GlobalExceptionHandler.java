f3dbb46b119ab942953e95fad15fd75fc141c9f1
kobumseouk <kbssky2993@gmail.com>
2025-07-14 17:59:01 +0900

    feat: GlobalException 구현

---
+++ src/main/java/com/teamEWSN/gitdeun/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.teamEWSN.gitdeun.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(GlobalException e) {
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+}
