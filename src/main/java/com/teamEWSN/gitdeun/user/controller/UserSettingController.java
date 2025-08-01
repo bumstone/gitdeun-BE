df107fe2e262af91500b5e6ce23efe77da628bdf
kobumseouk <kbssky2993@gmail.com>
2025-08-02 06:09:57 +0900

    fix: UserSettingController 엔드포인드 수정

--- src/main/java/com/teamEWSN/gitdeun/user/controller/UserSettingController.java
+++ src/main/java/com/teamEWSN/gitdeun/user/controller/UserSettingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/settings")
+@RequestMapping("/api/users/me/settings")
 @RequiredArgsConstructor
 public class UserSettingController {
     private final UserSettingService userSettingService;
