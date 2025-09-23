36bd4b0416729236e653ca2e4c1dda8144e110f2
kobumseouk <kbssky2993@gmail.com>
2025-09-24 00:00:23 +0900

    feat: CommentRepository 원상 복구

--- src/main/java/com/teamEWSN/gitdeun/comment/repository/CommentRepository.java
+++ src/main/java/com/teamEWSN/gitdeun/comment/repository/CommentRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @EntityGraph(attributePaths = {"attachments"})
+    @EntityGraph(attributePaths = {"user", "replies", "attachments"})
     List<Comment> findByCodeReviewId(Long reviewId);
 }
